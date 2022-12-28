22d60669733d17f05d95b6fd1fb1c469b63960a5
Jordan Wolter <jwolter456@gmail.com>
2022-12-27 19:44:23 -0500

    more styling done

--- filmfolio_front/src/components/HomeDetail.jsx
+++ filmfolio_front/src/components/HomeDetail.jsx
@@ -65,13 +65,19 @@ if(!movieDetail) {
           </div>
           
       </div>
+      
       <div className="ratingBox">
+      <div className="herocons"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg></div>
+      <div className="rated">
+        <h2>Your Rating:</h2>
+      </div>
         <ReactStars
             count={5}
             onChange={ratingChanged}
             size={24}
             color2={'#ffd700'} />
       </div>
+      
 
       </div>
       
