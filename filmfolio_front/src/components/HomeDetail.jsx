6ef8281992b295dc6bb17e16b87e377f1d5f4046
Jordan Wolter <jwolter456@gmail.com>
2023-01-04 16:10:57 -0500

    added delete for watchlist

--- filmfolio_front/src/components/HomeDetail.jsx
+++ filmfolio_front/src/components/HomeDetail.jsx
@@ -7,6 +7,7 @@ import CreateReview from "./CRUD/CreateReview"
 import DeleteReview from "./CRUD/DeleteReview"
 import UpdateReview from "./CRUD/UpdateReview"
 import WatchlistAdd from "./CRUD/WatchlistAdd"
+import WatchlistDelete from "./CRUD/WatchlistDelete"
 
 export default function Home (props) {
 
@@ -91,13 +92,15 @@ if(!movieDetail) {
       <div className="ratingBox ml-4 mt-4 ">
       <div className="rated flex justify-center gap-4">
         <div className="herocons hover:cursor-pointer">
-        <svg onClick={toggleClass} className={eye} class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+
+        <WatchlistAdd  watch={movieDetail}/>
+        
 
         <svg onClick={toggleClassOne} className={thumb} class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path></svg>
 
-        {/* <svg onClick={toggleClassTwo} className={clock} class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> */}
         
-        <WatchlistAdd  watch={movieDetail}/>
+        
+        <WatchlistDelete watch={movieDetail} />
         
         </div>
       </div>
