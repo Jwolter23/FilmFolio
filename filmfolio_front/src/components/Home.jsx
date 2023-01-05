efe92ed48fcae49839dfc6c0f8046ffef1b172d4
Jordan Wolter <jwolter456@gmail.com>
2023-01-05 15:59:21 -0500

    added Emotion and color seems to work

--- filmfolio_front/src/components/Home.jsx
+++ filmfolio_front/src/components/Home.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react"
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import WatchlistAdd from './CRUD/WatchlistAdd'
-
 export default function Home () {
 
   let navigate = useNavigate()
@@ -41,9 +40,21 @@ if(!movies) {
       <div onClick={() => showMovies(movies)} key={movies.title}
       className='card'>
         
-      <h2 className="movieTitle">{movies.title}</h2>
-      <div className="previewText">
-      <img className="Img" src={`${movies.photo_url}`}  />
+      <h2 className="movieTitle" style={{color: `${movies.color}`}}>{movies.title}</h2>
+      <div className="previewText" >
+
+      <img className="Img"  src={`${movies.photo_url}`} style={{
+  // border: "4px solid",
+  borderImage: `linear-gradient(135deg,${movies.color} 0 10px,${movies.color} 0 20px,${movies.color} 0 30px) 8`,
+  // WebkitMask: "conic-gradient(from 180deg at top 8px right 8px, #0000 90deg,#000 0) var(--_i,200%) 0  /200% var(--_i,8px) border-box no-repeat, conic-gradient(at bottom 8px left  8px,  #0000 90deg,#000 0) 0   var(--_i,200%)/var(--_i,8px) 200% border-box no-repeat, linear-gradient(#000 0 0) padding-box no-repeat",
+  // transition: ".3s, -webkit-mask-position .3s .3s",
+  // '&:hover': {
+  //   '--_i': '100%',
+  //   color: '#CC333F',
+  //   transition: '.3s, -webkit-mask-size .3s .3s',
+  // }
+
+}}></img>   
       </div>
       </div>
       ))}
