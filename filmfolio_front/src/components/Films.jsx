ae76ac6e3e17f11ab13d919dd61a32806f763d5e
Jordan Wolter <jwolter456@gmail.com>
2023-01-04 19:22:23 -0500

    got search working nothing rendered yet

--- filmfolio_front/src/components/Films.jsx
+++ filmfolio_front/src/components/Films.jsx
@@ -25,20 +25,17 @@ useEffect(()=>{
 
 }, [])
 
-const [movieSearch, setMovieSearch] = useState(null)
+// const [movieSearch, setMovieSearch] = useState(null)
 
 
-  const getSearch = async () =>{
-  const response = await axios.get('http://localhost:8000/movies/')
+//   const getSearch = async () =>{
+//   const response = await axios.get(`http://localhost:8000/movies/${movies.find(e => e.title === formState.search).title}`)
 
 
-  setMovies(response.data)
-  console.log(response.data)
+//   setMovieSearch(response.data)
+//   console.log(response.data)
 
-  }
-
-
-
+//   }
 
 const initialState = {
     search: '',
@@ -50,11 +47,35 @@ const initialState = {
     event.preventDefault()
     console.log(formState)
     setFormState(initialState)
+    searchMovie(formState.search);
   }
   
   const handleChange = (event) => {
     setFormState({...formState, [event.target.id]: event.target.value})
   }
+
+
+
+const searchMovie = async (title) => {
+    const movies = await axios.get(`http://localhost:8000/movies/`);
+    const movie = movies.data.find(movie => movie.title == title);
+    if (movie) {
+      console.log(movie);
+    } else {
+      console.log('Movie not found');
+    }
+  }
+  
+  
+
+
+
+
+
+
+
+
+
 
 if(!movies) {
   return <h2>Loading Home</h2>
@@ -66,19 +87,20 @@ if(!movies) {
       <form className='submitForm' 
       onSubmit={handleSubmit}>
         <label htmlFor='search'>Search Movies: </label>
-        <input type='text' 
+        <input className='inputSearch'type='text' 
         placeholder='Input Movie Title'
         id='search' 
         onChange={handleChange} 
         value={formState.search}>
         </input>
         <button type='submit' 
-        onClick={()=>getSearch()}>Search</button>
+        onClick={()=>searchMovie()}>Search</button>
         </form>
     </div>
 
 
-    
+
+
     <div className='grid'>
       {
       movies.map((movies)=>(
