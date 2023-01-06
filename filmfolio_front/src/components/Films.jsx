720fa56d5d021be5e999d1a76088a5ae6dba86c5
Jordan Wolter <jwolter456@gmail.com>
2023-01-05 20:45:20 -0500

    last commit

--- filmfolio_front/src/components/Films.jsx
+++ filmfolio_front/src/components/Films.jsx
@@ -78,7 +78,7 @@ if(!displayedMovies) {
   return(
     <div ref={animationParent}className='container'>
     <div className="title">
-      <h1 className="trending">Browse our selection</h1>
+      <h1 className="trending">Browse selection</h1>
       <div ref={animationParent}className="magnify">
       <svg ref={animationParent} onClick={() => setInputVisible(!inputVisible)}class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
       </div>
