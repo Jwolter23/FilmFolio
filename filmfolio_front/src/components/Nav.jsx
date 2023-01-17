6348fb700a17fb67d16ef1a3165ee54f33421d42
Jordan Wolter <jwolter456@gmail.com>
2023-01-17 16:09:43 -0500

    removed firebase for now commented out

--- filmfolio_front/src/components/Nav.jsx
+++ filmfolio_front/src/components/Nav.jsx
@@ -1,17 +1,17 @@
 import { Link } from 'react-router-dom'
 import {useAuthState} from 'react-firebase-hooks/auth'
-import {auth} from '../utils/firebase'
+// import {auth} from '../utils/firebase'
 import axios from 'axios'
 
 export default function Nav () {
-    const [user, loading] = useAuthState(auth)
+    // const [user, loading] = useAuthState(auth)
     return (
         <div>
             <div className='navBar'>
             <Link to='/' className='links'> Home </Link>
             <Link to='/profile' className='links'> Profile </Link>
             <Link to='/films' className='links'> Films </Link>
-            {!user && (
+            {/* {!user && (
             <Link to={'/auth/login'}>
                 <a className='py-2 px-4 text-lg  text-white rounded-lg font-medium ml-8 testing3'>
                     Sign In
@@ -25,7 +25,7 @@ export default function Nav () {
                         <img src={user.photoURL} alt='avatar' referrerPolicy='no-referrer' className='w-12 rounded-full' />
                     </Link>
                 </div>
-            )}
+            )} */}
             </div>
         
 
--- filmfolio_front/src/pages/auth/Login.jsx
+++ filmfolio_front/src/pages/auth/Login.jsx
@@ -1,34 +1,34 @@
-import {FcGoogle} from 'react-icons/fc';
-import {AiFillFacebook} from 'react-icons/ai'
-import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
-import {auth} from '../../utils/firebase'
+// import {FcGoogle} from 'react-icons/fc';
+// import {AiFillFacebook} from 'react-icons/ai'
+// import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+// import {auth} from '../../utils/firebase'
 
 
-export default function Login(){
-    //Sign in with google 
-    const googleProvider = new GoogleAuthProvider;
-    const GoogleLogin = async () => {
-        try {
-            const result = await signInWithPopup(auth,googleProvider)
-            console.log(result.user)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    return(
-        <div className='shadow-xl mt-32 p-10 text-grey700 rounded-lg'>
-            <h2 className='text-3xl font-medium'>Join today</h2>
-            <div className='py-4'>
-                <h3 className='py-4'>Sign in with one of the providers</h3>
-            </div>
-            <div className='flex flex-col gap-4'>
-                <button onClick={GoogleLogin}className='text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2'>
-                <FcGoogle className='text-2xl'/>Sign in with Google
-                </button>
-                <button className='text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2'>
-                    <AiFillFacebook className='text-2xl text-blue-400 '/>Sign in with Facebook
-                </button>
-            </div>
-        </div>
-    )
-}
+// export default function Login(){
+//     //Sign in with google 
+//     const googleProvider = new GoogleAuthProvider;
+//     const GoogleLogin = async () => {
+//         try {
+//             const result = await signInWithPopup(auth,googleProvider)
+//             console.log(result.user)
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
+//     return(
+//         <div className='shadow-xl mt-32 p-10 text-grey700 rounded-lg'>
+//             <h2 className='text-3xl font-medium'>Join today</h2>
+//             <div className='py-4'>
+//                 <h3 className='py-4'>Sign in with one of the providers</h3>
+//             </div>
+//             <div className='flex flex-col gap-4'>
+//                 <button onClick={GoogleLogin}className='text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2'>
+//                 <FcGoogle className='text-2xl'/>Sign in with Google
+//                 </button>
+//                 <button className='text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2'>
+//                     <AiFillFacebook className='text-2xl text-blue-400 '/>Sign in with Facebook
+//                 </button>
+//             </div>
+//         </div>
+//     )
+// }
